0d8e3e09022e2d929cee248a0f5438f5654376e7
Joe Ray <jray@xenzone.com>
2020-01-30 09:42:39 +0000

    Get it making queries

--- rplugin/python3/deoplete/sources/clubhouse.py
+++ rplugin/python3/deoplete/sources/clubhouse.py
@@ -14,36 +14,38 @@ class Source(Base):
         self.description = 'Clubhouse ticket numbers'
         self.mark = '[ch]'
         self.filetypes = ['gitcommit']
-        self.input_pattern = '[ch'
+        self.input_pattern = r'\[ch'
         self._token = None
         self._query = None
 
     def on_init(self, context):
         self._query = context['vars'].get('deoplete#sources#clubhouse#query', 'is:story')
         token_file = context['vars'].get('deoplete#sources#clubhouse#apitokenfile', '{}/.clubhouse'.format(os.getenv('HOME')))
-        print(token_file)
         if os.path.isfile(token_file):
             with open(token_file, 'r') as fh:
                 self._token = fh.read()
 
     def gather_candidates(self, context):
-        if self.token is None:
+        if self._token is None:
             return []
 
         payload = {
             'page_size': 10,
-            'query': self.query
+            'query': self._query
         }
 
-        url = urlparse.urlparse('{}?token={}'.format(self.url, self.token))
+        print(payload)
 
         c = http.client.HTTPSConnection('api.clubhouse.io')
-        c.request('GET', '/api/v3/search/stories?token={}'.format(self.token), body=json.dumps(payload), headers={'Accept': 'application/json'})
+        c.request('GET', '/api/v3/search/stories?token={}'.format(self._token), body=json.dumps(payload), headers={'Accept': 'application/json'})
 
         # try:
         response = c.getresponse()
-        print(response)
+        if response.status != 200:
+            print(dir(response))
+            return
         result = json.loads(response.read().decode('utf-8'))
+        print(result)
 
         return [{'word': '[ch{}]'.format(ticket['id']),
                  'menu': ticket['name']} for ticket in result['data']]
